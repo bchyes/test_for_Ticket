cc429dd42f6f22cdfe471990e2863f644c352bbe
bchyes <bchyes@126.com>
2022-05-29 10:24:04 +0800

    test

--- cmake-build-debug/CMakeFiles/code.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/code.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/mnt/d/CLion/Ticket？/main_new.cpp" "/mnt/d/CLion/Ticket？/cmake-build-debug/CMakeFiles/code.dir/main_new.cpp.o"
+  "/mnt/d/CLion/code/main_new.cpp" "/mnt/d/CLion/code/cmake-build-debug/CMakeFiles/code.dir/main_new.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /mnt/d/CLion/Ticket？
+# Install script for directory: /mnt/d/CLion/code
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/mnt/d/CLion/Ticket？/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/mnt/d/CLion/code/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
